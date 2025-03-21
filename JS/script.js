af08ac0b98db403c16b0419f0d3010852d34b806
Guilherme Macedo <guilhermecedoanastacio@gmail.com>
2025-03-21 13:54:50 -0300

    refinamento

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,48 @@
 let aleatorio = Math.floor(Math.random() * 100) + 1;
 let cont = 10;
+
 function tigrinho(e) {
   e.preventDefault();
-  const numero = parseInt(document.getElementById("numero").value);
   console.log(aleatorio);
-  if (numero == aleatorio) {
-    document.getElementById("dica").innerHTML = "Acertou!!";
-    reiniciar();
+  const inputElemento = document.getElementById("numero");
+  const numero = parseInt(inputElemento.value);
+
+  if (isNaN(numero) || numero < 1 || numero > 100) {
+    document.getElementById("dica").innerText =
+      "Digite um número válido entre 1 e 100!";
     return;
   }
-  if (cont == 0) {
-    document.getElementById("dica").innerText = "Você perdeu!!";
-    reiniciar();
+
+  if (numero === aleatorio) {
+    document.getElementById("dica").innerText =
+      "🎉 Acertou!! O jogo vai reiniciar...";
+    setTimeout(reiniciar, 2000);
     return;
   }
-  if (numero > aleatorio) {
-    document.getElementById("dica").innerText = "O número secreto é menor";
-    cont--;
-    return;
-  } else {
-    document.getElementById("dica").innerText = "O número secreto é maior";
-    cont--;
-    document.getElementById(
-      "tentativas"
-    ).innerText = `Tentativas Restantes: ${cont}`;
+
+  cont--;
+
+  if (cont === 0) {
+    document.getElementById("dica").innerText =
+      "😞 Você perdeu! O jogo vai reiniciar...";
+    setTimeout(reiniciar, 2000);
     return;
   }
+
+  document.getElementById("dica").innerText =
+    numero > aleatorio
+      ? "O número secreto é menor!"
+      : "O número secreto é maior!";
+
+  document.getElementById(
+    "tentativas"
+  ).innerText = `Tentativas Restantes: ${cont}`;
 }
+
 function reiniciar() {
   cont = 10;
   aleatorio = Math.floor(Math.random() * 100) + 1;
-  document.getElementById("tentativas").innerText = "O jogo irá reiniciar";
+  document.getElementById("tentativas").innerText = "Tentativas Restantes: 10";
+  document.getElementById("dica").innerText = "Dica:";
+  document.getElementById("numero").value = "";
 }
